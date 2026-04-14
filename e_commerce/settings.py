8c72e06fe3f3ffdc29c13bb9b661c68db12c7229
olatunwake <owolabiolatunjji@gmail.com>
2026-04-14 12:47:59 +0100

    debug settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fh+sav54n@#z14ihsr@bspa(yhss0qfo(jmz-#ow)+wkk_d1@0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
