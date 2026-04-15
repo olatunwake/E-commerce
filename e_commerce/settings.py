3a3cd2d464a60647d6817537adc4983eb6d25b0b
olatunwake <owolabiolatunjji@gmail.com>
2026-04-15 11:31:13 +0100

    settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fh+sav54n@#z14ihsr@bspa(yhss0qfo(jmz-#ow)+wkk_d1@0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
